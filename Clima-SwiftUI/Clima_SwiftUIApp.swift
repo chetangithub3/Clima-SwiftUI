e9871f506531c5aee4107707babc2ef7b97d566f
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2023-08-20 13:13:58 -0400

    Initial Commit

---
+++ Clima-SwiftUI/Clima_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clima_SwiftUIApp.swift
+//  Clima-SwiftUI
+//
+//  Created by Chetan Dhowlaghar on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Clima_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
